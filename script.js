eaadb4ac99974b35fe21766b59b284993d210538
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-03-13 21:21:25 +0300

    My submit button is functional and responsive

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-    $("cont").submit("click" , function(event){
+    $('#btn').on('click' , function(event){
       event.preventDefault();
+      var name = $("input#Name").val();
+      var email = $("input#email").val();
+      var message = $("input#message").val();
+      if (name && email){
+        alert ("HI " + name + ", we have received your message and will get back to you as soon as possible.");
+      } 
+      else {
+        alert("Please enter your name and email!");
+      }
+      //console.log(message);
+    })});
+
+        
+    
